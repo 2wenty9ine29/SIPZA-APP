1278d0810ec87a4f826bd8413c74d69131ba416e
2wenty9ine29 <stephenbaaye123456ab@gmail.com>
2026-09-08 19:00:03 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = "price-list-sales-calculator-v5-5-1-r3";
-const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
-self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
-self.addEventListener("fetch", event => { if (event.request.method !== "GET") return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
